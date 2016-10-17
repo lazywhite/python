0d6b14308f79f023edd25af725c3ca61fb3938a1
white <346816483@qq.com>
2016-10-17 15:49:14 +0800

    update note for slack

---
+++ tools/send_slack.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+from __future__ import print_function
+import requests
+import sys
+import json
+'''
+# get the webapi url 
+    login Slack.com
+    Browse Apps
+        Custom Integration
+            Incoming WebHooks
+                https://nexttaodev.slack.com/services/B2KELQPPU
+# post a json formatted payload
+'''
+url = "https://hooks.slack.com/services/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"
+
+channel = "#alert"
+username = "zabbix"
+icon_emoji = ":ghost:"
+logfile = '/tmp/send_slack.log'
+
+if __name__ == '__main__':
+
+    subject = str(sys.argv[1])
+    message = str(sys.argv[2])
+
+    content = "%s\n\n%s" % (subject, message)
+    
+    post = {}
+    payload = {}
+    payload['channel'] = channel
+    payload['icon_emoji'] = icon_emoji
+    payload['username'] = username
+    payload['text'] = content
+    post['payload'] = json.dumps(payload)
+    with open(logfile, 'a') as log:
+        try:
+            resp = requests.post(url, data=post)
+            print(resp.content, file=log)         
+        except Exception as e:
+            print(e, file=log)
+
