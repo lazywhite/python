d41d99a2f79f97d87e6a364ea29871b453d84128
white <346816483@qq.com>
2016-10-17 16:22:21 +0800

    update for slack alert

--- tools/send_slack.py
+++ tools/send_slack.py
@@ -4,15 +4,7 @@ from __future__ import print_function
 import requests
 import sys
 import json
-'''
-# get the webapi url 
-    login Slack.com
-    Browse Apps
-        Custom Integration
-            Incoming WebHooks
-                https://nexttaodev.slack.com/services/B2KELQPPU
-# post a json formatted payload
-'''
+
 url = "https://hooks.slack.com/services/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"
 
 channel = "#alert"
@@ -22,11 +14,12 @@ logfile = '/tmp/send_slack.log'
 
 if __name__ == '__main__':
 
-    subject = str(sys.argv[1])
-    message = str(sys.argv[2])
+    to = str(sys.argv[1])
+    subject = str(sys.argv[2])
+    message = str(sys.argv[3])
 
-    content = "%s\n\n%s" % (subject, message)
-    
+    content = "%s: %s\n\n%s" % (to, subject, message)
+
     post = {}
     payload = {}
     payload['channel'] = channel
@@ -37,7 +30,6 @@ if __name__ == '__main__':
     with open(logfile, 'a') as log:
         try:
             resp = requests.post(url, data=post)
-            print(resp.content, file=log)         
+            print(resp.content, file=log)
         except Exception as e:
             print(e, file=log)
-
