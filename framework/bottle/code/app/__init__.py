d19e0eecc44e3b3400b427d6868e812f47f46968
white <346816483@qq.com>
2016-12-24 18:10:37 +0800

    update session support for bottle app

--- framework/bottle/code/app/__init__.py
+++ framework/bottle/code/app/__init__.py
@@ -1,27 +1,64 @@
-from bottle import  Bottle, view, template
+from bottle import  Bottle, view, template, request, response, redirect
 import bottle_mysql as bm
+import bottle_session
+import bottle_redis
+import redis
 
 #import pdb
 
 app = Bottle()
 
 #pdb.set_trace()
-app.config.load_config('config.ini')
+#app.config.load_config('config.ini')
 #plugin = bm.Plugin(dbhost=app.config['mysql']['dbhost'], dbuser='root', dbpass='root', dbname='test')
-plugin = bm.Plugin(dbuser='root', dbpass='root', dbname='test')
-app.install(plugin)
+sqlplugin = bm.Plugin(dbuser='root', dbpass='root', dbname='test')
+app.install(sqlplugin)
+
+session_plugin = bottle_session.SessionPlugin(cookie_lifetime=600)
+
+redis_plugin = bottle_redis.RedisPlugin()
+conn_pool = redis.ConnectionPool(host='localhost', port=6379)
+
+session_plugin.connection_pool = conn_pool
+redis_plugin.redisdb = conn_pool
+app.install(session_plugin)
+app.install(redis_plugin)
 
 
-
-
-@app.route('/user/<uid>')
-def user(uid, db):
-    db.execute('select name from user where id=%s', (uid,))
-
+@app.route('/login', method='POST')
+def login(db, session):
+    postbody = request.body.read()
+    print postbody
+    username = request.forms.get("username")
+    password = request.forms.get("password")
+    db.execute("select password from user where name=%s", (username,))
     row = db.fetchone()
-    if row:
-        name = row.get('name')
+    if row and (password == row.get("password")):
+        session['username'] = username
+        redirect('/login')
     else:
-        name = 'guest'
-    return template('user', name=name)
+        return "password not correct"
+
+@app.route('/login', method='GET')
+def login(session):
+    username = session.get('username')
+    if username:
+        redirect('/user')
+    else:
+        return template('login')
+
+@app.route('/logout', method='GET')
+def logout(session):
+    del(session['username'])
+#    return 'logout'
+    redirect("/login")
+
+
+@app.route('/user')
+def user(db, session):
+    username = session.get('username')
+    if not username:
+        redirect("/login")
+    else:
+        return template('user', name=username)
 
---
+++ framework/bottle/code/views/login.tpl
@@ -0,0 +1,5 @@
+<form method="POST" action='/login'>
+<input name="username" type="text" />
+<input name="password" type="text" />
+<input type="submit" value="Submit"/>
+</form>
