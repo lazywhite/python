ae343a078dba6d5be24a69aeee80d6649b683817
white <346816483@qq.com>
2017-07-02 16:02:18 +0800

    python 元类

--- metaclass/metaclass.py
+++ metaclass/metaclass.py
@@ -1,35 +1,61 @@
+# -*- coding: utf-8 -*-
 '''
-    create a class as metaclass
-    then define it's __new__ method
-    at last use type to create and return a class
+元类可以是任何callable的对象, 不限于function, class, 或是type, 只要能接受
+(clsName, parents, attrs) 并返回一个类对象, 就叫元类
+
+class XX:
+    pass
+实际会调用metaclass创建一个类实例, 默认是type
+
+python2.7
+    手动指定元类
+        class Foo:
+            __metaclass__ = xx
+    全局设置
+        __metaclass__ = xx
+
+python3.x
+    class Foo(Bar, metaclass=XX):
+        pass
 '''
 
-from __future__ import print_function 
+'''
+1. 用函数作为元类
+'''
 def upper_attr(future_class_name, future_class_parents, future_class_attr):
     attrs = ((name, value) for name, value in future_class_attr.items() 
             if not name.startswith('__'))
     uppercase_attr = dict((name.upper(), value) for name, value in attrs)
     return type(future_class_name, future_class_parents, uppercase_attr)
 
-#__metaclass__ = upper_attr
 
-class Foo(metaclass=upper_attr):
-    foo = 'biu'
+class Foo:
+    foo = 'foo'
+
+class Bar(Foo):
+    __metaclasss__ = upper_attr
+    bar = "bar"
+
+print dir(Bar)
 
 
-print(dir(Foo))
-
-
+'''
+2. 用类作为元类
+'''
 class UpperAttrMetaclass(type):
     def __new__(cls, name, bases, dct):
         attrs = ((name, value) for name, value in dct.items() if not name.startswith('__'))
         uppercase_attr=dict((name.upper(),value) for name, value in attrs)
+        print "metaclass name: ", cls
+        print "class name: ", name
+        print "class parents: ", bases
+        print "class attrs: " , dct
         return super(UpperAttrMetaclass, cls).__new__(cls, name, bases, uppercase_attr)
 
 
 
-class Bar(metaclass=UpperAttrMetaclass):
-#    __metaclass__ = UpperAttrMetaclass
+class Bar(object):
+    __metaclass__ = UpperAttrMetaclass
     bar = 'bar'
 
-print(dir(Bar))
+print dir(Bar)
