84a1ad356b511e0ae258f8abeee25f7bbebf22ed
white <346816483@qq.com>
2017-07-02 15:33:25 +0800

    python descriptor protocol

--- descriptor/descriptor.py
+++ descriptor/descriptor.py
@@ -3,14 +3,17 @@ class UpperString(object):
         self._value = ''
         
     def __get__(self,instance,cls):
+        print "context: ", id(instance)," ", cls.__name__
         print 'value accessed'
         return self._value
 
     def __set__(self,instance,value):
+        print "context: ", id(instance)," ", value
         print 'value setted'
         self._value = str(value).upper()
 
     def __delete__(self, instance):
+        print "context: ", id(instance)
         print 'deleted'
 
 
@@ -19,6 +22,7 @@ class MyClass(object):
 
 
 a = MyClass()
-a.attribute = 'he'
+print "id of object a", id(a)
+a.attribute = 'attribute value'
 print a.attribute
 del a.attribute
--- descriptor/property.py
+++ descriptor/property.py
@@ -4,30 +4,47 @@ class MyClass(object):
 
 
     def _i_get(self):
+        print "getter"
         return self._secret_thing
 
     def _i_set(self, value):
+        print "setter"
         self._secret_thing = value
 
     def _i_delete(self):
-        print('neh !!')
+        print "deleter"
 
-    my_thing = property(_i_get, _i_set, _i_delete, 'the thing')
+    my_thing = property(_i_get, _i_set, _i_delete, 'property description')
 
 
 class C(object):
     def __init__(self,name,age):
         self.name = name
         self._x = age
+
     @property
     def x(self): 
-#        raise AttributeError #self._x
+        print 'x getter'
         return self._x
+
     @x.setter
-    def x(self, value): self._x = value
+    def x(self, value): 
+        print 'x setter'
+        self._x = value
+
     @x.deleter
     def x(self):
+        print 'x deleter'
         del self._x
+
+a = MyClass()
+print a.my_thing
+a.my_thing = 10
+print a.my_thing
+del(a.my_thing)
 
 c=C('mary',20)
 print(c.x)
+c.x = 100
+print(c.x)
+del(c.x)
