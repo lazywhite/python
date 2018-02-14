6a55801fa8f907c92b4282fb3d34ed485553bc11
white <346816483@qq.com>
2018-02-14 16:57:51 +0800

    update for logging

---
+++ logging/run.py
@@ -0,0 +1,48 @@
+import logging
+
+# filter
+# logger.addFilter()
+# format
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+f_handler = logging.FileHandler('./test.log',mode='a')
+s_handler = logging.StreamHandler()
+
+f_handler.setFormatter(formatter)
+s_handler.setFormatter(formatter)
+level = logging.DEBUG 
+logger = logging.getLogger("main")
+logger.setLevel(level)
+logger.addHandler(f_handler)
+logger.addHandler(s_handler)
+
+logger.warning('warning')
+
+def main():
+   logging.basicConfig(
+           filename = '/var/log/app.log',
+           level = logging.ERROR,
+           format = '%(levelname)s:%(asctime)s:%(message)s'
+           )
+   logger = logging.getLogger()
+   hostname = 'www.python.org'
+   item = 'spam'
+   filename = 'data.csv'
+   mode = 'r'
+
+   logger.critical('host %s unknow', hostname)
+   logger.error('couldn\'t find %r', item)
+   logger.warning('feature is deprecated')
+   logger.info('opening file %r, mode=%r', filename, mode)
+   logger.debug('got here')
+
+
+   '''
+   log exception stack
+   try:
+       1/0
+   except:
+       logger.exception("error") # 必须传入一个参数
+   '''
+
+if __name__ == '__main__':
+   main()
