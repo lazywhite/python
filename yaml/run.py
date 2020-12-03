937ea81c9a83aec9a559d5e92ddf0eb0476183e5
lazywhite <346816483@qq.com>
2020-12-03 11:35:46 +0800

    update yaml note

--- yaml/run.py
+++ yaml/run.py
@@ -1,5 +1,12 @@
 import yaml
 
+data = {}
+
 with open('et.yml','r+') as f:
-    data = yaml.load(f)
+    data = yaml.safe_load(f)
     print(data)
+
+data['test'] = "vs"
+
+with open('et.yml','wt') as f:
+    yaml.dump(data, f)
