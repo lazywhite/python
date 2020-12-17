6dffbc0933f2c0f4a46f30b32c4c72f15b293c48
lazywhite <346816483@qq.com>
2020-12-17 10:08:50 +0800

    yaml加入多行内容

--- yaml/run.py
+++ yaml/run.py
@@ -1,12 +1,16 @@
 import yaml
+from pprint import pprint
 
 data = {}
 
 with open('et.yml','r+') as f:
     data = yaml.safe_load(f)
-    print(data)
+
+pprint(data)
+pprint(data.get("foo"))
+pprint(data.get("bar"))
 
 data['test'] = "vs"
 
-with open('et.yml','wt') as f:
-    yaml.dump(data, f)
+#with open('et.yml','wt') as f:
+#    yaml.dump(data, f)
